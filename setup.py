80061dce06efee23edfffdd454351da32ae724a3
Jarod Mica <jarodmica@gmail.com>
2023-08-16 03:43:03 -0700

    small changes

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, find_packages
 
-# Read the requirements.txt file
-with open('requirements.txt', 'r') as f:
-    requirements = f.read().splitlines()
-
 setup(
     name='rvc_tts_pipe',
     version='0.1',
